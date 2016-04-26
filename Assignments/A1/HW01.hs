112c3879d5db9246b5e7f26973ee6c79f3d96989
tyuijhgs <Mr.Christopher.Blackman@gmail.com>
2016-04-26 14:54:07 -0400

    semi working assignment

---
+++ Assignments/A1/HW01.hs
@@ -0,0 +1,53 @@
+{-# OPTIONS_GHC -Wall #-}
+module HW01 where
+
+-- Exercise 1 -----------------------------------------
+
+-- Get the last digit from a number
+lastDigit :: Integer -> Integer
+lastDigit n = n `mod` 10
+
+-- Drop the last digit from a number
+dropLastDigit :: Integer -> Integer
+dropLastDigit n = ((n - (lastDigit n)) `div` 10)
+
+-- Exercise 2 -----------------------------------------
+
+toRevDigits :: Integer -> [Integer]
+toRevDigits n
+  | n <= 0 = []
+  | otherwise = (lastDigit n):(toRevDigits (dropLastDigit n))
+
+-- Exercise 3 -----------------------------------------
+
+-- Double every second number in a list starting on the left.
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther (x:[]) = [(2*x)]
+doubleEveryOther (x:(y:z)) = (x):(2*y):(doubleEveryOther z)
+
+-- Exercise 4 -----------------------------------------
+
+-- Calculate the sum of all the digits in every Integer.
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:y)
+  | x < 10 = x + (sumDigits y) 
+  | otherwise = sumDigits (toRevDigits x)
+
+-- Exercise 5 -----------------------------------------
+
+-- Validate a credit card number using the above functions.
+luhn :: Integer -> Bool
+luhn n
+  | n >= 0 = ((sumDigits (doubleEveryOther (toRevDigits n))) `mod` 10 ) == 0
+  | otherwise = False
+
+-- Exercise 6 -----------------------------------------
+
+-- Towers of Hanoi for three pegs
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi = undefined
---
+++ Assignments/A1/Main.hs
@@ -0,0 +1,4 @@
+Module Main(main) where
+import HW01
+
+main = print(luhn(12121))
